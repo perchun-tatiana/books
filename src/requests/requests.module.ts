a53259838155a255f4abf8aeb52dad8425c64249
perchun-tatiana <tatiana.perchun.freelancer@gmail.com>
2024-06-07 14:19:51 +0300

    add auth module to request module to use authguard

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -3,9 +3,10 @@ import { RequestController } from './request/request.controller';
 import { PrismaRequestService } from './prisma-requests/prisma-requests.service';
 import { PrismaService } from 'src/requests/prisma.service';
 import { MailModule } from '../mail/mail.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, AuthModule],
   controllers: [RequestController],
   providers: [PrismaService, PrismaRequestService],
 })
